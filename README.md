704fcf3ab0339a7969e65f436e90777dbea2be7f
Mika van Os <os0017@hz.nl>
2025-12-10 10:39:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Hello World 🌍
+
+## Who am I?
+
+My name is Mika, I'm currently a first year student at my university, learning all about Information and Communication Technologies (ICT)  
+I go by He/Him or They/Them pronouns and I prefer dogs over cats 🐕  
+
+## What brought me here?
+
+This GitHub profile was initially created purely for the projects related to this course, but as was only natural I've sort of fallen in love with it and will be using this page for personal projects, too.
+
+## What am I working on?
+
+As mentioned above, most of the comomits here will be related to me working on projects for Uni, but perhaps you'll see creative projects pop up too. 
+
+Here's a list of languages and Frameworks I've been familiarizing myself with so far, though!
+
+### Languages:
+- HTML
+- CSS
+- JavaScript
+- TypeScript
+- PHP
+- Python
+
+### Frameworks:
+- Laravel
+- TailwindCSS
+- BulmaCSS
+
+## What do I hope to learn?
+
+Front-End developing is currently really interesting to me. I want to learn to work with more frameworks to bring my ideas to life within websites and projects.
+
+### I look forward to working with you all! ✨
---
+++ components/navigation.js
@@ -0,0 +1,43 @@
+export class Navigation extends HTMLElement {
+    connectedCallback() {
+        const nav = document.createElement("nav");
+        const title = document.createElement("h2");
+
+        title.innerHTML = "&#9655; Navigation";
+        nav.appendChild(title);
+
+        const pages = [
+            { name: "Home", id: "home", link: "/home", route: "/"},
+            { name: "About me", id: "about", link: "/about", route: "/about" },
+            { name: "Socials", id: "socials", link: "/socials", route: "/socials" },
+        ];
+
+        for (const page of pages) {
+            const button = document.createElement("button");
+
+            button.id = page.id;
+            button.innerHTML = `<h3>${page.name}</h3>`;
+
+            button.addEventListener("click", function() {
+            Render(`./pages${page.link}.html`)
+        });
+
+            nav.appendChild(button);
+        }
+        nav.classList.add("box")
+        this.appendChild(nav);
+    }
+    
+};
+
+function Render(page){
+    const content = document.getElementById('content')
+            fetch(page).then(function (res) {
+                if (res.ok) {
+                    return res.text();
+                }
+                throw res;
+            }).then(function (text) {
+                content.innerHTML = text;
+            });
+}
