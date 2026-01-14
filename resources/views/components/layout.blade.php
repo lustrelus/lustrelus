61c9a255d0914703bfe0d6c91a4497b2c05cca23
Mika van Os <os0017@hz.nl>
2026-01-14 16:40:01 +0100

    General updates

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,10 @@
     <meta name="description" content="The website of LustreLus" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <script src="{{URL::asset('storage/js/app.js')}}" type="module">
+    <script src="{{asset('storage/js/app.js')}}" type="module">
     </script>
 
-    <link rel="stylesheet" href="{{URL::asset('storage/css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('storage/css/app.css')}}">
 
     <link rel="icon" href="data:image/svg+xml,
     <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
@@ -20,7 +20,7 @@
 
 <body>
     <header>
-        <img src="{{URL::asset('storage/img/banner.png')}}">
+        <img src="{{ asset('storage/img/banner.png') }}">
     </header>
 
     <main>
